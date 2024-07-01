8ea32873af45c0ddf2a393151841243ffe93faed
kazxk <152793459+kazxk@users.noreply.github.com>
2024-07-01 13:36:14 +0300

    Add files via upload

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { ColorModeScript } from "@chakra-ui/react";
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>ORION - Post Wall</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
